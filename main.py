8fb17b88eafc945aac4aece3d82f994ed9082602
kamabc <38250302+kamabc@users.noreply.github.com>
2020-04-02 13:13:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# link の形式
+# {line_id:{no:no, state:state, param:param, condition:{conditions}}}
+
 # インポートしよう。
 from flask import Flask, request, abort
 from linebot import(
@@ -13,16 +16,6 @@ import os
 import json
 import datetime
 import re
-
-
-# 学生の皆さん
-class Student:
-    def __init__(self, line, no=None, state='linking', param=0, condition=[]):
-        self.line = line
-        self.no = no
-        self.state = state
-        self.param = param
-        self.condition = condition
 
 
 # ファイル類
@@ -46,11 +39,9 @@ choice_questions = [
             '体がだるい、重い等の症状はありますか？',
             '37.5℃以上の発熱がありますか？',
             'いつもとは違った息苦しさがありますか？',
-            '一緒に住んでいる家族の中で、具合の悪い人はいますか？'
+            '一緒に住んでいる家族の中で、具合の悪い人はいますか？',
             ]
-
-# 変数
-students = []
+tempr_questions = ['今の体温を摂氏(℃)単位で入力して下さい。小数点以下1位までお願いします。\n(例)36.2℃の場合\n 36.2 と入力'']
 
 @app.route('/callback', methods=['Post'])
 def callback():
@@ -79,24 +70,18 @@ def handle_message(event):
     with open(LINKS_JSON, 'r', encoding='utf-8') as f:
         links = json.load(f)
 
-        for link in list(links.items()):
-            student = Student(link[0], link[1], 'linked')
-            students.append(student)
-
     # jsonの中になかったらとりま
     if not(user_id in links.keys()):
-        student = Student(user_id)
+        links[user_id] = {'no':None, 'state':'linking', 'param':0, 'conditions':{}}
 
-    # 返信するとこ
-    for student in students:
-        if student.line == user_id: break
+    # ここで変数
+    user_info = links[user_id]
 
-    # studentのstateで分岐
+    # 生徒のstateで分岐
     # 出席番号危機だす
-    if student.state == 'linking':
+    if user_info['state'] == 'linking':
         # 番号
         user_no = list(map(lambda str: re.sub(r'\D', '', str), user_msg.split('-')))
-        print(user_no)
 
         # 有効な入力
         if (len(user_no) == 3) and ((user_no[0]+user_no[1]+user_no[2]).isdigit()) and (1 <= int(user_no[0]) <= 3) and (1 <= int(user_no[1]) <= 6) and (1 <= int(user_no[2]) <= 40):
@@ -108,17 +93,12 @@ def handle_message(event):
 
             # 登録
             else:
-                student.no = user_no
-                student.state = 'linked'
-                links[student.line] = student.no
+                user_info['no'] = user_no
+                user_info['state'] = 'linked'
                 msg = '登録が完了しました。'
 
                 with open(LINKS_JSON, 'w', encoding='utf-8') as f:
                     json.dump(links, f)
-
-        # 無効な入力
-        elif not(len(user_no) == 3) or not((user_no[0]+user_no[1]+user_no[2]).isdigit()):
-            msg = '存在しない出席番号です。\nもう一度、有効な出席番号を入力してください。'
 
         else:
             msg = '出席番号を、次の(例)のように入力してください。\n(例):「1年C組27番」の場合\n 1-3-27 と入力\n(例):「2年E組9番」の場合\n 2-5-9と入力'
@@ -129,6 +109,19 @@ def handle_message(event):
             TextSendMessage(text=msg)
         )
 
+    # リンクしてるとき
+    elif user_info['state'] == 'linked':
+        if (user_msg == '体調チェック') and (user_info['param'] % 100 == 0):
+            # 時刻によって分岐
+            if 4 <= now.hour <= 9:
+                msg = '朝の健康チェックを開始します。'
+                user_info['param'] += 1
+
+            elif 11 <= now.hour <= 13:
+                msg = '昼の健康チェックを開始します。'
+                user_info['param'] += 1
+
+            # 朝のやつ
 
 
 
